816610856f8bbed34462c0667a9a643f7b185326
quan2907 <108460265+quan2907@users.noreply.github.com>
2022-06-30 16:42:47 +0700

    Add files via upload

---
+++ baimang.php
@@ -0,0 +1,10 @@
+<html>
+<?php
+$person= array("name"=>"Kien", "job"=>"Teacher",
+"email"=>"minh1605@gmail.com","age"=>"38");
+foreach($person as $key=>$test)
+{
+echo "Key: $key. Gia Tri: $test<br>";
+}
+?>
+</html>
---
+++ baimang1.php
@@ -0,0 +1,7 @@
+<html>
+<?php
+$name= array("Mai", "Dung", "Hung", "Kien");
+foreach ($name as $test)
+{ echo "$test<br>"; }
+?>
+</html>
